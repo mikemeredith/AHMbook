cd9999f556cf1dbc0217276683fddf00d732500e
Mike Meredith <mmeredith@wcsmsia>
2017-04-29 19:24:12 +0800

    Updates for sim.spatialDS

--- R/sim-spatialDS_9-8-3_Simulate_spatial_DS.R
+++ R/sim-spatialDS_9-8-3_Simulate_spatial_DS.R
@@ -7,7 +7,7 @@
 #   (introduced in Section 9.8.3)
 
 sim.spatialDS <- function(N=1000, beta = 1, sigma=1, keep.all=FALSE,
-  B=3, model=c("halfnorm", "hazard"), show.plot=TRUE){
+  B=3, model=c("logit", "halfnorm", "hazard"), lambda = B/3, show.plot=TRUE){
 # Function simulates coordinates of individuals on a square
 # Square is [0,2B] x [0,2B], with a count location on the point (B, B)
 #   N: total population size in the square
@@ -24,7 +24,7 @@ grx <- seq(delta/2, 2*B - delta/2, delta) # mid-point coordinates
 gr <- expand.grid(grx,grx)         # Create grid coordinates
 
 # Create spatially correlated covariate x and plot it
-V <- exp(-e2dist(gr,gr)/1)
+V <- exp(-e2dist(gr,gr)/lambda)
 x <- t(chol(V))%*%rnorm(900)
 
 
@@ -46,26 +46,31 @@ if(model=="hazard")
    p <- 1-exp(-exp(-d*d/(2*sigma*sigma)))
 if(model=="halfnorm")
    p <- exp(-d*d/(2*sigma*sigma))
+if(model=="logit")
+   p<- 2*plogis(  -d*d/(2*sigma*sigma)   )
 # Now we decide whether each individual is detected or not
 y <- rbinom(N, 1, p)                                           # detected or not
 
 if(show.plot) {
   op <- par(mar=c(3,3,3,6)) ; on.exit(par(op))
-  image(rasterFromXYZ(cbind(as.matrix(gr),x)), col=topo.colors(10)) # need to convert gr to a matrix
-  draw.circle(3, 3, B)
-  points(3, 3, pch="+", cex=3)
+  image(rasterFromXYZ(cbind(as.matrix(gr),x)), col=topo.colors(10), asp=1, bty='n') # need to convert gr to a matrix
+  rect(0, 0, 2*B, 2*B)  # draw box around the image
+  draw.circle(B, B, B)
+  points(B, B, pch="+", cex=3)
   image_scale(x, col=topo.colors(10))
   points(u1, u2, pch=20, col='black', cex = 0.8)  # plot points
   title("Extremely cool figure")         # express your appreciation of all this
-  points(u1[d <= B], u2[d <= B], pch = 16, col = "black", cex = 1) # not detected
+  points(u1[d <= B], u2[d <= B], pch = 16, col = "black", cex = 1) # in circle but not detected
   points(u1[y==1], u2[y==1], pch = 16, col = "red", cex = 1)     # detected
 }
-# Put all of the data in a matrix ## matrix? what matrix?
+# Remove data for individuals not detected
 if(!keep.all){
    u1 <- u1[y==1]
    u2 <- u2[y==1]
-   d <- d[y==1]   }
+   d <- d[y==1]
+   pixel.id <- pixel.id[y==1]   }
 # Output
-return(list(model=model, N=N, beta=beta, B=B, u1=u1, u2=u2, d=d, y=y, N.real=N.real, Habitat=x, grid=gr))
+return(list(model=model, N=N, beta=beta, B=B, u1=u1, u2=u2, d=d, pixel.id=pixel.id, 
+  y=y, N.real=N.real, Habitat=x, grid=gr))
 }
 
