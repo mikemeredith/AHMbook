68251c6c24e4b7e2e2c5f6581bb901a0a40d4934
Mike Meredith <mmeredith@wcsmsia>
2018-12-03 20:09:53 +0800

    Added zinit function

---
+++ R/zinit.R
@@ -0,0 +1,22 @@
+
+# Generate starting values for survival analysis in JAGS or WinBUGS
+
+# AHM2 section 14.2.3
+
+zinit  <- function(CH){
+  CH <- round(as.matrix(CH)) # could also be a data frame
+
+  f <- suppressWarnings(apply(CH, 1, function(x) min(which(x!=0)))) # occasion of first capture
+  zinit <- array(NA, dim = dim(CH))
+  for(i in 1:nrow(CH)){
+    if(f[i] >= ncol(CH)) # first captured on last occasion (or never!)
+      next
+    zinit[i,(f[i]+1):ncol(CH)] <- 1
+  }
+  return(zinit)
+}
+
+
+
+
+
