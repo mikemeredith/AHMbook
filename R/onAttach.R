c490095e123b8681e774a8c5ec1b005b0be795c6
Mike Meredith <mmeredith@wcsmsia>
2019-05-13 10:17:34 +0800

    Changes to DESCRIPTION

--- R/onAttach.R
+++ R/onAttach.R
@@ -4,5 +4,5 @@
   version <- try(packageVersion('AHMbook'), silent=TRUE)
   if(!inherits(version, "try-error"))
     packageStartupMessage("This is AHMbook ", version,
-      ". For overview type ?AHMbook.")
+      ". For overview type ?AHMbook; for changes do news(p='AHMbook').")
 }
