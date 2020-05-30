58d295f6f53a5141caddca3e617ce3b5a0e92517
mikemeredith <mmeredith@wcs.org>
2020-05-30 12:51:47 +0800

    Tweaks to Authors@R

--- .local_checks/@making_AHMbook_package.R
+++ .local_checks/@making_AHMbook_package.R
@@ -20,13 +20,13 @@ install.packages(c("plotrix", "raster", "RandomFields", "coda",
 
 unlink(list.files(pattern="Rplots.pdf", recursive=TRUE))
 system("R CMD build AHMbook")  # Produces the .tar.gz
-# system("R CMD check AHMbook_0.1.4.9107.tar.gz --no-manual")
-system("R CMD check --run-donttest AHMbook_0.1.4.9107.tar.gz")
-# system("R CMD check --as-cran AHMbook_0.1.4.9107.tar.gz")
-# system("R CMD check --as-cran AHMbook_0.1.4.9107.tar.gz --no-manual")
+# system("R CMD check AHMbook_0.1.4.9109.tar.gz --no-manual")
+system("R CMD check --run-donttest AHMbook_0.1.4.9109.tar.gz")
+# system("R CMD check --as-cran AHMbook_0.1.4.9109.tar.gz")
+# system("R CMD check --as-cran AHMbook_0.1.4.9109.tar.gz --no-manual")
 # Sys.setenv(R_ZIPCMD = "C:/Rtools/bin/zip.exe")
-system("R CMD INSTALL --build AHMbook_0.1.4.9107.tar.gz") # installs and produces the .zip binary
-system("R CMD INSTALL --build --with-keep.source AHMbook_0.1.4.9107.tar.gz") # installs and produces the .zip binary
+system("R CMD INSTALL --build AHMbook_0.1.4.9109.tar.gz") # installs and produces the .zip binary
+system("R CMD INSTALL AHMbook_0.1.4.9109.tar.gz") # installs only
 
 system("R CMD INSTALL AHMbook") # Use this for a "dev" install.
 
