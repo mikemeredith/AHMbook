2cab39a1db60d66b9f7ad4436a739875db084e78
mikemeredith <mike@mmeredith.net>
2021-05-16 13:33:05 +0800

    Roll-up to 0.2.3

--- R/modSelFP.R
+++ R/modSelFP.R
@@ -3,16 +3,21 @@
 
 # Named fp.modSel in earlier drafts.
 
-modSelFP <- function(mod.list){   # Thanks to Chris Sutherland!
-   # (the same of the 'one-s**t hypothesis')
-  Model <- names(mod.list)
-  nPars <- sapply(mod.list, function(x)length(x@opt$par))
-  nll <- sapply(mod.list, function(x)x@opt$value)
-  AIC <- round(sapply(mod.list, function(x)x@AIC),2)
-  dAIC <- round(AIC - min(AIC), 2)
-  AICwt <- round(exp(-0.5 * dAIC)/sum(exp(-0.5 * dAIC)), 2)
-  modTab <- data.frame(nPars, AIC, dAIC, AICwt, row.names = Model)[order(AIC),]
-  modTab$cuWt <- cumsum(modTab$AICwt)  # Do this after sorting.
-  return(modTab)
+# modSelFP <- function(mod.list){   # Thanks to Chris Sutherland!
+  # (the same of the 'one-s**t hypothesis')
+  # Model <- names(mod.list)
+  # nPars <- sapply(mod.list, function(x)length(x@opt$par))
+  # nll <- sapply(mod.list, function(x)x@opt$value)
+  # AIC <- round(sapply(mod.list, function(x)x@AIC),2)
+  # dAIC <- round(AIC - min(AIC), 2)
+  # AICwt <- round(exp(-0.5 * dAIC)/sum(exp(-0.5 * dAIC)), 2)
+  # modTab <- data.frame(nPars, AIC, dAIC, AICwt, row.names = Model)[order(AIC),]
+  # modTab$cuWt <- cumsum(modTab$AICwt)  # Do this after sorting.
+  # return(modTab)
+# }
+
+modSelFP <- function(mod.list){
+  message("Please use the functions in 'unmarked' for this:\n  modSel(fitList(fits=mod.list))")
+  unmarked::modSel(unmarked::fitList(fits=mod.list))
 }
 
--- R/onAttach.R
+++
@@ -1,8 +0,0 @@
-
-
-.onAttach <- function(libname, pkgname) {
-  version <- try(packageVersion('AHMbook'), silent=TRUE)
-  if(!inherits(version, "try-error"))
-    packageStartupMessage("This is AHMbook ", version,
-      ". For overview type ?AHMbook; for changes do news(p='AHMbook').")
-}
