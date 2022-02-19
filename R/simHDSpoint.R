5c7fd7789a215e19abefdbcd6b5f13e4d5ab330b
mikemeredith <mike@mmeredith.net>
2022-02-19 09:32:12 +0800

    More tweaks to simHDSpoint

--- R/simHDSpoint.R
+++ R/simHDSpoint.R
@@ -8,8 +8,8 @@
 
 # Function to simulate data under hierarchical distance sampling protocol point transects
 
-simHDSpoint  <- function(nsites = 100, mean.density = 1,
-   beta.density  = 1, mean.sigma = 20, beta.sig = -5, B = 50, discard0=FALSE, show.plots=TRUE){
+simHDSpoint  <- function(nsites = 1000, mean.density = 1,
+   beta.density  = 1, mean.sigma = 20, beta.sigma = -5, B = 60, discard0=FALSE, show.plots=TRUE){
 #
 # Function simulates hierarchical distance sampling (HDS) data under
 #   a point transect protocol.
@@ -19,7 +19,7 @@ simHDSpoint  <- function(nsites = 100, mean.density = 1,
 #     mean.density: expected DENSITY, number of individuals per HECTARE
 #     beta.density: coefficient of log(expected density) on habitat covariate
 #     mean.sigma: scale parameter sigma in the half-normal detection function in METERS
-#     beta.sig: slope of log-linear regression of scale parameter on wind speed
+#     beta.sigma: slope of log-linear regression of scale parameter on wind speed
 #     B: maximum distance from the point, in METERS.
 #     discard0: if TRUE, sites with no detections will be removed from the output.
 #     show.plots: if TRUE, plots of the output are displayed
@@ -40,10 +40,10 @@ lambda <- exp(log(mean.density) + beta.density*habitat) * base::pi * B^2 / 1e4 #
 N <- rpois(nsites, lambda)                  # site-specific abundances
 
 # Detection probability model (site specific)
-sigma <- exp(log(mean.sigma) + beta.sig*wind)
+sigma <- exp(log(mean.sigma) + beta.sigma*wind)
 
 # Simulate observation model
-dataList <- vector(mode = "list", length = nsites)
+dataList <- Nlist <- vector(mode = "list", length = nsites)
 counts <- numeric(nsites)
 
 for(i in 1:nsites){
@@ -53,17 +53,18 @@ for(i in 1:nsites){
   }
   # Simulation of distance from point given uniform distribution on the circle (algorithm of Wallin)
   d <- sqrt(runif(N[i], 0, 1)) * B
+  Nlist[[i]] <- d
 
   # Detection process, half-normal detection function
   p <- exp(-d^2 / (2 * (sigma[i]^2)))  # Detection probability ..
-  y <- rbinom(N[i], 1, p)              # Det./non-detection of each individual
-  counts[i] <- sum(y)
+  det <- rbinom(N[i], 1, p)              # Det./non-detection of each individual
+  counts[i] <- sum(det)
   # Subset to "captured" individuals only
-  d <- d[y==1]
+  d <- d[det==1]
   if(length(d) == 0)
     d <- NA
 
-  # Compile things into a list
+  # Plug the d's into the list together with site IDs.
   dataList[[i]] <- cbind(site=i, d=d)
 }
 
@@ -72,31 +73,31 @@ data <- do.call(rbind, dataList)
 
 # Subset to sites at which individuals were captured. You rarely
 #  want to do this depending on how the model is formulated so be careful.
-dataNoNA <- data[!is.na(data[,2]),]
 if(discard0)
-  data <- dataNoNA
+  data <- data[!is.na(data[,2]),]
 
 # Visualisation
 if(show.plots) {
-
-  angle <- runif(nrow(data), 0, 2*base::pi)
-  u <- data[, 'd'] * cos(angle)
-  v <- data[, 'd'] * sin(angle)
-
   op <- par(mfrow = c(2,2)) ; on.exit(par(op))
   tryPlot <- try( {
-    plot(u, v, pch = 16, main =
-      "Located individuals in point transects", xlim = c(-B, B),
-      ylim = c(-B, B), col = data[,1], asp = 1)
+
+    angle <- runif(nrow(data), 0, 2*base::pi)
+    x <- data[, 'd'] * cos(angle)
+    y <- data[, 'd'] * sin(angle)
+    plot(x, y, pch = 16, main =
+        "Locations of detected individuals", xlim = c(-B, B),
+        ylim = c(-B, B), col = data[,1], asp = 1)
     points(0, 0, pch = "+", cex = 3, col = "black")
     plotrix::draw.circle(0, 0, B)
-    hist(data[,"d"], col = "lightblue", breaks = 20, xlim=c(0, B),
-        main = "Frequency of distances", xlab = "Distance")
-    ttt <- table(dataNoNA[,1])
-    n <- rep(0, nsites)
-    n[as.numeric(rownames(ttt))] <- ttt
-    plot(habitat, n, main = "Observed counts (n) vs. habitat")
-    plot(wind, n, main = "Observed counts (n) vs. wind speed")
+
+    tmp <- hist(unlist(Nlist), breaks = 20, xlim=c(0, B), xlab = "Distance",
+        main = "Frequency of distances\nblue: detected, gray: undetected")
+    hist(data[,"d"], breaks = tmp$breaks,
+        col = "lightblue", add=TRUE)
+
+    plot(habitat, counts, main = "Observed counts (n) vs. habitat")
+
+    plot(wind, counts, main = "Observed counts (n) vs. wind speed")
   }, silent = TRUE)
   if(inherits(tryPlot, "try-error"))
     tryPlotError(tryPlot)
@@ -106,9 +107,8 @@ if(show.plots) {
 # Output
 list( # arguments input
     nsites = nsites, mean.density = mean.density, beta.density = beta.density,
-    mean.sigma = mean.sigma, beta.sig = beta.sig, B = B,
+    mean.sigma = mean.sigma, beta.sigma = beta.sigma, B = B,
     # generated values
-    data = data, counts = counts, habitat = habitat,
-    wind = wind, N = N )
+    data = data, counts = counts, habitat = habitat, wind = wind, N = N )
 }
 
